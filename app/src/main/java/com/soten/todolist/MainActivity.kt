f0a1c9c54cf2684e686dcf5f7cec331a662bf3e5
Choiyounho <maxosa78@gmail.com>
2021-03-02 19:18:03 +0900

    [feat] : 이메일 로그인 구현

--- app/src/main/java/com/soten/todolist/MainActivity.kt
+++ app/src/main/java/com/soten/todolist/MainActivity.kt
@@ -1,23 +1,48 @@
 package com.soten.todolist
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.soten.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val RC_SIGN_IN = 3000
+
     private lateinit var binding: ActivityMainBinding
 
     private val viewModel: TodoViewModel by viewModels()
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        auth = Firebase.auth
+
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build())
+
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN)
+        }
 
         binding.recyclerview.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
@@ -40,6 +65,20 @@ class MainActivity : AppCompatActivity() {
         viewModel.todoLiveData.observe(this@MainActivity, Observer {
             (binding.recyclerview.adapter as TodoAdapter).setData(it)
         })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+            } else {
+                finish()
+            }
+        }
     }
 
 }
